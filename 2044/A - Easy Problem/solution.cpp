eb21ace84f92187cedaf8a51b5b15c3d0883146a
Gabriel Holanda <gholanda.alb@gmail.com>
2026-07-09 23:51:26 -0300

    Add Easy Problem [A] from Codeforces

---
+++ 2044/A - Easy Problem/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int cases;
+    cin >> cases;
+    for (int i = 0; i < cases;i++)
+    {
+        int a;
+        cin >> a;
+        cout << a-1 << endl;
+    }
+}
