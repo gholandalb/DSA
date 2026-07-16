66e37b3ac77e9c13be423822c6f7b6a2a14ad742
Gabriel Holanda <gholanda.alb@gmail.com>
2026-07-16 11:25:37 -0300

    07-16-2026  0217-contains-duplicate  Easy

---
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        bool works = false;
+        unordered_set<int> s;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (s.count(nums[i]) == 1)
+            {
+                works = true;
+                break;
+            }
+            s.insert(nums[i]);
+        }
+        return works;
+    }
+};
