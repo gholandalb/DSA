444658f8a24fb4438ed69b5b378ce44db481c51e
Gabriel Holanda <gholanda.alb@gmail.com>
2026-09-04 20:44:42 -0300

    09-04-2026  3903-smallest-stable-index-i  Easy

--- LeetCode/Easy/3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ LeetCode/Easy/3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -3,10 +3,16 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
         int max_num = nums[0];
+        vector<int> smallest_num(n);
+        smallest_num[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+        {
+            smallest_num[i] = min(smallest_num[i + 1], nums[i]);
+        }
         for (int i = 0; i < n; i++)
         {
             max_num = max(max_num, nums[i]);
-            if (max_num - *min_element(nums.begin() + i, nums.end()) <= k)
+            if (max_num - smallest_num[i] <= k)
             {
                 return i;
             }
